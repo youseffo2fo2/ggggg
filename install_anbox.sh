caec76850055eac789e146c9fb54135a0927ed6a
youseffo2fo2 <87866141+youseffo2fo2@users.noreply.github.com>
2022-11-26 08:46:41 +0200

    Create install_anbox.sh

---
+++ install_anbox.sh
@@ -0,0 +1,5 @@
+sudo apt install openjdk-16-jre # installing Java Enviroment
+sudo snap install --devmode --beta anbox # installing anbox
+sudo apt install dkms # installing ubuntu 20.04 dkms modules because currently ppa is disabled by default from Ubuntu security
+sudo apt install linux-headers-generic # installing the headers
+sudo snap install render # installs a render lib which helps anbox of rendering the windows and that's why some of you got issues when running from command line or it was super slow
